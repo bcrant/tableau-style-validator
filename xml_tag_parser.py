8f5bd2becfcfbbb643e16a44b0d8f8b3e5240871
Brian Crant <bcrant@gmail.com>
2021-05-08 18:55:43 -0700

    Parse first top level parent (Worksheet - Title Formatting)

--- xml_tag_parser.py
+++ xml_tag_parser.py
@@ -1,21 +1,53 @@
 from bs4 import BeautifulSoup
-
+import lxml
+import pprint
 
 infile = open('tableau_style_tags.xml', 'r')
 contents = infile.read()
-soup = BeautifulSoup(contents, 'xml') # Optionally use 'lxml' here
+soup = BeautifulSoup(contents, 'lxml')   # Optionally use 'lxml' here
 # print(soup.prettify())
 
 #
 # FORMAT CONTAINER 1: WORKSHEETS
 #
 worksheets = soup.find_all('worksheet')
+
+ws = {}
 for worksheet in worksheets:
-    print(worksheet['name'])
-    print(worksheet.prettify(), '\n\n')
+    #
+    # WORKSHEETS - Title Formatting
+    #
+    ws['name'] = worksheet['name']
+
+    ws['title'] = None
+    if worksheet.find('layout-options') is not None:
+        runs = worksheet\
+            .find('layout-options')\
+            .find('title')\
+            .find('formatted-text')\
+            .findAll('run', recursive=False)
+
+        title = []
+        title_font_attributes = []
+        for run in runs:
+            title.append(run.text)
+            title_font_attributes.append({k: v for k, v in run.attrs.items()})
+        ws['title'] = title[0]
+        ws['title_font_attributes'] = list(filter(None, title_font_attributes))
+
+    pprint.pprint(ws)
+    print('\n\n')
+
+    # print(worksheet.findChildren('layout-options'))
+    # print(type(worksheet.findChildren('layout-options')))
+
+    # print(worksheet.find('layout-options').next_siblings)
+    # .find('title').find('formatted-text'))
+
+    # print(worksheet.find_all(.prettify(), '\n\n')
+    # print(layout-options><title><formatted-text>)
 
 
-# #
 # # PATHS WE WANT
 # #
 # # WORKSHEET <workbook><worksheets><worksheet>
