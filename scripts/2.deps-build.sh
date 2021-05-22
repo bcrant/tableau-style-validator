9f115a84bd3380a6cffd82f2dd750f42b41510b4
Brian Crant <bcrant@gmail.com>
2021-05-22 11:17:07 -0700

    Add local AWS env requirements.

--- scripts/2.deps-build.sh
+++ scripts/2.deps-build.sh
@@ -7,8 +7,10 @@ cd lambda-deps
 rm -rf alldeps.zip
 
 rm -rf python && mkdir -p python
-docker run --rm -v $(pwd):/tmp -w /tmp amazon/aws-lambda-python:latest --platform linux/arm64/v8 pip install -r requirements-deps.txt -t ./python;
+docker run --rm -v $(pwd):/tmp -w /tmp lambci/lambda:build-${PYTHON_VERSION} bash -c \
+  "arch && whoami && pip install -r requirements-deps.txt -t ./python";
+
 echo -e "ZIPPING LAYER ARTIFACTS..."
 zip -q -r alldeps.zip . -x "*.pyc" "*.git" "*.sh" env.env out.json .DS_Store
 
-#rm -rf python
+rm -rf python
