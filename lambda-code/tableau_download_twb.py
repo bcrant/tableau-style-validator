bdb28c7e912e3dbb95719f5dd19952c5c02b6458
Brian Crant <bcrant@gmail.com>
2021-05-23 11:02:11 -0700

    Finalize local downloading of workbook. Begin testing through remote invocation.

--- lambda-code/tableau_download_twb.py
+++ lambda-code/tableau_download_twb.py
@@ -1,86 +1,60 @@
 import os
 import zipfile
-import requests
-from io import StringIO, BytesIO
 from dotenv import load_dotenv
 import tableauserverclient as TSC
-
-#
-# Call this script with payload of Tableau Webhook
-# Sample Payload:
-#
-# site_luid   f6a2b550-8b37-45c3-957e-99702e3dc5f6
-# resource    WORKBOOK
-# resource_luid   e03412ce-ab0f-4ca2-8ce8-fa9976737a68
-# resource_name   TestWebhook
-# event_type  WorkbookCreated
-# created_at  2021-05-21T04:12:08.662Z
 
 
 def init_env():
     if os.getenv('AWS_EXECUTION_ENV') is None:
-        print('Operating in local/dev context, loading ../.env file...')
+        print('Operating in local dev context, loading ./envs/.env file...')
         load_dotenv('./envs/.env')
     else:
         print('Operating in Lambda context...')
 
 
-def _encode_for_display(text):
-    """
-    Encodes strings so they can display as ASCII in a Windows terminal window.
-    This function also encodes strings for processing by xml.etree.ElementTree functions.
-    Returns an ASCII-encoded version of the text.
-    Unicode characters are converted to ASCII placeholders (for example, "?").
-    """
-    return text.encode('ascii', errors="backslashreplace").decode('utf-8')
-
-
-def authenticate_tableau():
+def download_workbook():
     init_env()
-
-    # Import Tableau environment variables          # EXAMPLE VALUES:
-    USERNAME = os.getenv('TABLEAU_USER')
-    PASS = os.getenv('TABLEAU_PASS')
-    SITE_NAME = os.getenv('TABLEAU_SITE_NAME')      # site-name AKA content-url
-    SERVER_URL = os.getenv('TABLEAU_SERVER_URL')    # https://10ay.online.tableau.com
+    #
+    # Import Tableau environment variables
     # For further explanation of Tableau environment variables visit:
     # https://tableau.github.io/server-client-python/docs/api-ref#tableauauth-class
+    #                                               # EXAMPLE VALUES:
+    USERNAME = os.getenv('TABLEAU_USER')            # you@email.com
+    PASS = os.getenv('TABLEAU_PASS')                # password
+    SITE_NAME = os.getenv('TABLEAU_SITE_NAME')      # site-name AKA content-url
+    SERVER_URL = os.getenv('TABLEAU_SERVER_URL')    # https://10ay.online.tableau.com
 
-    # (For local testing... will need to get these from the Zapier Webhook invocation JSON Payload)
-    TABLEAU_SITE_LUID = os.getenv('SITE_LUID')
+    # (For local testing... will need to get this from the Zapier Webhook invocation JSON Payload)
     TABLEAU_RESOURCE_LUID = os.getenv('RESOURCE_LUID')
 
-    print("Talking to Tableau...\n")
+    print("Signing into Tableau Server and fetching Workbook...")
     tableau_auth = TSC.TableauAuth(USERNAME, PASS, SITE_NAME)
-    print(tableau_auth)
     server = TSC.Server(SERVER_URL)
-    print(server)
-
     server.use_server_version()
-    print(server.version)
 
-    # Signing into Tableau Server and fetching Workbook
     with server.auth.sign_in(tableau_auth):
-
         # Select specific workbook with Resource LUID from Webhook payload
         wb = server.workbooks.get_by_id(TABLEAU_RESOURCE_LUID)
 
-        print(f'Downloading {wb.name} from {SERVER_URL}/{SITE_NAME}')
-        wb_file = server.workbooks.download(wb.id, include_extract=False)
+        print(f'Downloading "{wb.name}" from Tableau Server')
+        zipped_wb_path = server.workbooks.download(wb.id, include_extract=True)
 
-        # Files download as twbx in zipped form...
-        print(wb_file)
-        zipped_twbx = zipfile.ZipFile(wb_file)
-        print(zipped_twbx.namelist())
-        print(zipped_twbx.infolist())
+        # .twbx files are zipped archives. Here we extract the Workbook (.twb) only.
+        unzipped_wb_path = None
+        with zipfile.ZipFile(zipped_wb_path) as packaged_workbook:
+            for wb_file in packaged_workbook.namelist():
+                if '.twb' in wb_file:
+                    unzipped_wb_path = packaged_workbook.extract(wb_file)
+                    print(f'Extracting "{wb_file}" from {wb.name}.')
+                else:
+                    print(f'No Tableau Workbook file found in {wb.name}')
 
-        for z in zipped_twbx.namelist():
-            print(z)
-            if '.twb' in z:
-                print(z)
-        #         zipped_twbx.extract(z)
-        # zipped_twbx.close()
+        # Pass Tableau Workbook to parser as open file
+        with open(unzipped_wb_path) as f:
+            wb_f = f.read()
+
+        return wb_f
 
 
 if __name__ == "__main__":
-    authenticate_tableau()
+    download_workbook()
